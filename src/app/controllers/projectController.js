b91cecfd993a789e7c2d75d3f5efd66709846c1d
Syond <mandarksyond@hotmail.com>
2020-03-02 22:57:24 -0300

    Implementados: create, read, delete.

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -11,27 +11,70 @@ router.use(authMiddleWare);
 
 //Rota para listar todos
 router.get('/', async (req, res) => {
-    res.send({ ok: true, user: req.userId });
+
+    try {
+        const projects = await Project.find().populate('usuario');
+
+        return res.send({ projects });
+
+    } catch (error) {
+        return res.status(400).send({ error: "Error ao carrega projetos" });
+    }
+
 });
 
 //Rota para listar 1
 router.get('/:projectId', async (req, res) => {
-    res.send({ user: req.userId });
+
+    try {
+        const project = await Project.findById(req.params.projectId).populate('usuario');
+
+        return res.send({ project });
+
+    } catch (error) {
+        return res.status(400).send({ error: "Error ao carrega projeto" });
+    }
+
 });
 
 //Rota para criar
 router.post('/', async (req, res) => {
-    res.send({ user: req.userId });
+
+    const { titulo } = req.body;
+
+    try {
+        //Verificar: Não consegue cadastrar mesmo nome utilizando usuário diferente. Talvez tenha que testar o ID do usuário também. 
+        if(await Project.findOne({ titulo }))
+            return res.status(400).send({ error: "Projeto já cadastrado" });
+    
+        //Esse "req.userId" vem do middleware de autenticação
+        const project = await Project.create( { ...req.body, usuario: req.userId });
+
+        return res.send({ project });
+
+    } catch (error) {
+        res.status(400).send({ error: "Erro ao criar o projeto" });
+    }
+
 });
 
-//Rota para atualizar
+//Rota para atualizar - TODO
 router.put('/:projectId', async (req, res) => {
     res.send({ user: req.userId });
 });
 
 //Rota para deletar
 router.delete('/:projectId', async (req, res) => {
-    res.send({ user: req.userId });
+
+    try {
+        await Project.findByIdAndRemove(req.params.projectId);
+
+        return res.send({ msg: "Projeto removido com sucesso" });
+
+    } catch (error) {
+        return res.status(400).send({ error: "Error ao carrega projeto" });
+    }
+
 });
 
 
